96beac2aa5f26907ba0430adb100c908efd3c76a
أحمد المحمودي (Ahmed El-Mahmoudy) <aelmahmoudy@sabily.org>
2014-03-01 21:46:40 +0200

    Added basic gogoc settings to shared preferences (not used yet by app)

--- src/com/googlecode/gogodroid/GogoPreferenceActivity.java
+++ src/com/googlecode/gogodroid/GogoPreferenceActivity.java
@@ -2,15 +2,26 @@ package com.googlecode.gogodroid;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 
-public class GogoPreferenceActivity extends PreferenceActivity {
+public class GogoPreferenceActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
     getPreferenceManager().setSharedPreferencesName(Constants.PREFS_FILE);
 		addPreferencesFromResource(R.xml.preferences);
+    getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 	}
+
+  @Override
+  public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
+    if(key.equals("auth_method")) {
+      findPreference("userid").setEnabled(!sharedPreferences.getString("auth_method", "anonymous").equals("anonymous"));
+      findPreference("passwd").setEnabled(!sharedPreferences.getString("auth_method", "anonymous").equals("anonymous"));
+    }
+  }
 
 /*	@Override
 	protected void onDestroy() {

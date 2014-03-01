faa1a422ab99b685914380df8fd8eaa8782f5d3f
أحمد المحمودي (Ahmed El-Mahmoudy) <aelmahmoudy@sabily.org>
2014-03-01 21:57:57 +0200

    Simplified enable/disable logic for userid/passwd entries by using Preference.OnPreferenceChangeListener instead of the overhead of registering/unregistering a SharedPreferences.OnSharedPreferenceChangeListener

--- src/com/googlecode/gogodroid/GogoPreferenceActivity.java
+++ src/com/googlecode/gogodroid/GogoPreferenceActivity.java
@@ -2,30 +2,24 @@ package com.googlecode.gogodroid;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 
-public class GogoPreferenceActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+public class GogoPreferenceActivity extends PreferenceActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
     getPreferenceManager().setSharedPreferencesName(Constants.PREFS_FILE);
 		addPreferencesFromResource(R.xml.preferences);
-    getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+    findPreference("auth_method").setOnPreferenceChangeListener(
+      new Preference.OnPreferenceChangeListener() {
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+          findPreference("userid").setEnabled(!((String) newValue).equals("anonymous"));
+          findPreference("passwd").setEnabled(!((String) newValue).equals("anonymous"));
+          return(true);
+        }
+    });
 	}
-
-  @Override
-  public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
-    if(key.equals("auth_method")) {
-      findPreference("userid").setEnabled(!sharedPreferences.getString("auth_method", "anonymous").equals("anonymous"));
-      findPreference("passwd").setEnabled(!sharedPreferences.getString("auth_method", "anonymous").equals("anonymous"));
-    }
-  }
-
-/*	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-	}*/
-
 }
